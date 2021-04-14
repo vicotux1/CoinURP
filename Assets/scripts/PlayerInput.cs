a6c83d3eb38ba0abb973a3efac206c1f9ded71b6
DarioMirandaYT <66534526+vicotux1@users.noreply.github.com>
2021-04-14 14:50:26 -0300

    Old Input

--- Assets/scripts/PlayerInput.cs
+++ Assets/scripts/PlayerInput.cs
@@ -1,30 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerInput : MonoBehaviour{
-    private Rigidbody rb;
+    
+    
+    [SerializeField] PlayerMove Player;
+
+    [Header("Movement")]
+    public string AxisHorizontal;
+    public string AxisVertical;
+    [Header("Position")]
     public Vector3 Position_initial;
-    [Range(1.0f, 200.0f)]public float speed=10.0f;
     private float MovX, MovY;
+    
+    
+
+    
 
     private void Awake() {
-        rb=GetComponent<Rigidbody>();
-        Cursor.visible = false;
-        transform.position=Position_initial;
+    Cursor.visible = false;
+    transform.position=Position_initial;
     }
 
-    void OnMovement(InputValue MovementValue){
-        Vector2 MoveVector=MovementValue.Get<Vector2>();
-        MovX=MoveVector.x;
-        MovY=MoveVector.y;
-         
-        
+    void FixedUpdate() {
+    MovX= Input.GetAxis(AxisHorizontal);
+    MovY=Input.GetAxis(AxisVertical);
+    Player.OnMovement(MovX, MovY);     
 }
-private void FixedUpdate() {
-    Vector3 move=new Vector3(MovX,transform.position.y,MovY);
-    rb.AddForce(move*speed);
-}
+
 
 }
---
+++ Assets/scripts/PlayerMove.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour{
+   private Rigidbody rb;
+   [Range(1.0f, 200.0f)]public float speed;
+    private void Awake() => rb = GetComponent<Rigidbody>();
+
+
+    public void OnMovement(float MovX, float MovY){
+    Vector3 move=new Vector3(MovX,transform.position.y,MovY);
+    rb.AddForce(move*speed);
+    }
+}
